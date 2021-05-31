28ecb120ffde2573d3e10625c5e91e890364c5e6
Le Duc Pham <ducphamle212@gmail.com>
2021-05-31 23:46:28 +0700

    update message to hash instead of hello world & fixed private key

--- oraicli/cmd/staking/cmd/claim-commission.js
+++ oraicli/cmd/staking/cmd/claim-commission.js
@@ -2,6 +2,7 @@ import { Argv } from 'yargs';
 import Cosmos from '@oraichain/cosmosjs';
 import bech32 from 'bech32';
 import * as ed from 'noble-ed25519';
+import crypto from 'crypto';
 
 declare var cosmos: Cosmos;
 const address = "orai1whsvq9m56tr945xcnjd527p3wpm00m6sqgum57";
@@ -10,13 +11,25 @@ export default async (yargs: Argv) => {
 
     // Create a private key
 
-    const privateKey = ed.utils.randomPrivateKey(); // 32-byte Uint8Array or string.
-    const msgHash = Buffer.from('Hello World');
+    const privateKey = new Uint8Array([91, 76, 13, 131, 127, 103, 132, 13,
+        134, 176, 248, 242, 127, 217, 39, 60,
+        93, 36, 142, 98, 228, 197, 120, 35,
+        181, 87, 125, 184, 37, 4, 38, 19]);
+    console.log("private key: ", privateKey);
+    const msg = {
+        nft_addr: "orai1um7dwaz4uexd2zjl0yzeaqw20ltq7y5qpcq35n",
+        token_id: "1009",
+        orai_addr: "orai14n3tx8s5ftzhlxvq0w5962v60vd82h30rha573",
+        nonce: 1
+    }
+    let hash = crypto.createHash('sha256').update(JSON.stringify(msg)).digest('hex');
+    console.log("msg hash: ", hash);
+    let msgHash = Buffer.from(hash);
     (async () => {
         const publicKey = await ed.getPublicKey(privateKey);
-        console.log("public key; ", publicKey);
+        console.log("public key; ", Buffer.from(publicKey).toString('base64'));
         const signature = await ed.sign(msgHash, privateKey);
-        console.log("signature: ", signature)
+        console.log("signature: ", Buffer.from(signature).toString('base64'));
         const isSigned = await ed.verify(signature, msgHash, publicKey);
         console.log("is signed: ", isSigned);
     })();

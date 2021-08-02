33995c1dfa65dcb9a1b050e8c8dc93eb0e2ac283
Duc Pham Le <ducphamle212@gmail.com>
2021-08-02 16:46:09 +0700

    add simple request vrf v2

--- oraicli/cmd/drand/index.js
+++ oraicli/cmd/drand/index.js
@@ -3,5 +3,6 @@ export default async (yargs: Argv) => {
     yargs
         .usage('usage: $0 account <command>')
         .command('demo', 'demo drand with tx and queries', require('./cmd/demo').default)
-        .command('get-pubkey', 'get pubkey of vrf members through Oraichain', require('./cmd/getPubKey').default)
+        .command('get-pubkey', 'get pubkey of vrf members through Oraichain', require('./cmd/get-pubkey').default)
+        .command('simple-request', 'simple request for vrf Oraichain', require('./cmd/simple-request').default)
 }

379e2326f08f8f3874d3871d4015a45dd46c87f2
tuorai <tubackkhoa@gmail.com>
2021-09-26 11:38:50 +0700

    add key method for account

--- oraicli/cmd/account/index.js
+++ oraicli/cmd/account/index.js
@@ -3,7 +3,7 @@ export default async (yargs: Argv) => {
   yargs
     .usage('usage: $0 account <command>')
     .command('create', 'create account', require('./cmd/create').default)
-    .command('privatekey', 'get private key', require('./cmd/privatekey').default)
+    .command('key', 'get private key', require('./cmd/key').default)
     .command('balance', 'get account balance', require('./cmd/balance').default)
     .command('drand', 'create decentralized randomness', require('./cmd/drand').default)
     .command('oraichain-vrf', 'full flow creating a decentralized random seed', require('./cmd/oraichain-vrf').default);

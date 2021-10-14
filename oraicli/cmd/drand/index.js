7ca1da6e8d123773d2f4f4f528b312dd75fa04a9
piggy2303 <nguyentuananh23031997@gmail.com>
2021-10-14 04:38:39 +0000

    add new dir for marketplace interaction

--- oraicli/cmd/drand/index.js
+++ oraicli/cmd/drand/index.js
@@ -1,8 +1,8 @@
 import { Argv } from 'yargs';
 export default async (yargs: Argv) => {
     yargs
-        .usage('usage: $0 account <command>')
+        .usage('usage: $0 drand <command>')
         .command('demo', 'demo drand with tx and queries', require('./cmd/demo').default)
         .command('get-pubkey', 'get pubkey of vrf members through Oraichain', require('./cmd/get-pubkey').default)
-        .command('simple-request', 'simple request for vrf Oraichain', require('./cmd/simple-request').default)
+        .command('get-members', 'get vrf members through Oraichain', require('./cmd/get-members').default);
 }
--- oraicli/cmd/marketplace/index.js
+++ oraicli/cmd/marketplace/index.js
@@ -1,10 +1,7 @@
 import { Argv } from 'yargs';
 export default async (yargs: Argv) => {
   yargs
-    .usage('usage: $0 wasm <command> [options]')
-    .command('query', 'query a smart contract', require('./cmd/query-smart').default)
-    .command('execute', 'execute a smart contract', require('./cmd/execute').default)
-    .command('deploy', 'deploy a smart contract', require('./cmd/deploy').default)
+    .usage('usage: $0 marketplace <command> [options]')
     .command('update-storage', 'update storage', require('./cmd/update-storage').default)
     .command('update-royalties', 'update royalties', require('./cmd/update-royalties').default)
     .command('query-pagination', 'update storage', require('./cmd/query-pagination').default)

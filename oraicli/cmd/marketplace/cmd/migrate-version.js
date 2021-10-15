be6895fe518632a7f41065cc2011d445a15a20c8
piggy2303 <nguyentuananh23031997@gmail.com>
2021-10-15 06:03:11 +0000

    add migrate version script

--- oraicli/cmd/marketplace/cmd/migrate-version.js
+++ oraicli/cmd/marketplace/cmd/migrate-version.js
@@ -1,5 +1,3 @@
-// THIS FILE RE-CALCULATE THE CREATOR & PROVIDER ROYALTIES TO MATCH THE NEW LOGIC OF THE MARKETPLACE
-
 import { Argv } from 'yargs';
 import Cosmos from '@oraichain/cosmosjs';
 
@@ -25,37 +23,27 @@ const getHandleMessage = (contract, msg, sender, amount) => {
     });
 };
 
-const getRoyalties = async (address, nftAddr) => {
+const getNfts = async (owner, address) => {
     let offset = null;
     let url = cosmos.url;
-    let royalties = [];
+    let nfts = [];
     let data = {};
     do {
         const input = JSON.stringify({
-            msg: {
-                get_royalties_contract: {
-                    contract_addr: nftAddr,
-                    offset: offset,
-                    order: 1
-                }
+            tokens: {
+                owner,
+                start_after: offset
             }
         });
-        console.log("input: ", input);
         console.log(`${url}/wasm/v1beta1/contract/${address}/smart/${Buffer.from(input).toString('base64')}`);
-        data = await fetch(`${url}/wasm/v1beta1/contract/${address}/smart/${Buffer.from(input).toString('base64')}`).then(data => data.json());
-        royalties = royalties.concat(data.data);
-        console.log("data: ", data.data);
-        console.log("royalties: ", royalties[royalties.length - 1]);
-        offset = {
-            contract: royalties[royalties.length - 1].contract_addr,
-            token_id: royalties[royalties.length - 1].token_id,
-            creator: royalties[royalties.length - 1].creator,
-        };
-    } while (data.data.length > 0);
-    royalties = royalties.filter(
-        (v, i, a) => a.findIndex((t) => t.token_id === v.token_id) === i
-    );
-    return royalties;
+        data = await cosmos.get(`/wasm/v1beta1/contract/${address}/smart/${Buffer.from(input).toString('base64')}`);
+        nfts = nfts.concat(data.data.tokens);
+        console.log("data: ", data.data.tokens);
+        console.log("nfts: ", nfts[nfts.length - 1]);
+        offset = nfts[nfts.length - 1];
+    } while (data.data.tokens.length > 0);
+    nfts = [...new Set(nfts)];
+    return nfts;
 }
 
 export default async (yargs: Argv) => {
@@ -79,19 +67,19 @@ export default async (yargs: Argv) => {
     const childKey = cosmos.getChildKey(argv.mnemonic);
     const sender = cosmos.getAddress(childKey);
 
-    let royalties = await getRoyalties(address, argv.nftaddr);
-    royalties = royalties.map(r => ({ ...r, royalty: r.royalty * Math.pow(10, 7) > Math.pow(10, 9) ? r.royalty : r.royalty * Math.pow(10, 7) }));
-    // royalties = royalties.filter(r => r.creator_type === "provider");
-    console.log("royalties: ", JSON.stringify(royalties));
-    console.log("royalty length: ", royalties.length);
+    let nfts = await getNfts(address, argv.nftaddr);
+    console.log("nfts: ", JSON.stringify(nfts));
+    console.log("nft length: ", nfts.length);
     const input = Buffer.from(JSON.stringify({
-        update_royalties: {
-            royalty: royalties,
+        migrate_version: {
+            nft_contract_addr: argv.nftaddr,
+            token_ids: nfts,
+            new_marketplace: argv.market
         }
     }));
 
-    // update royalties
-    const txBody = getHandleMessage(argv.market, input, sender, argv.amount);
+    // update nfts
+    const txBody = getHandleMessage(address, input, sender, argv.amount);
     try {
         const res = await cosmos.submit(childKey, txBody, 'BROADCAST_MODE_BLOCK', isNaN(argv.fees) ? 0 : parseInt(argv.fees), argv.gas);
         console.log(res);
--- oraicli/cmd/marketplace/index.js
+++ oraicli/cmd/marketplace/index.js
@@ -5,6 +5,7 @@ export default async (yargs: Argv) => {
     .command('update-storage', 'update storage', require('./cmd/update-storage').default)
     .command('update-royalties', 'update royalties', require('./cmd/update-royalties').default)
     .command('query-pagination', 'update storage', require('./cmd/query-pagination').default)
+    .command('migrate-version', 'migrate marketplace version', require('./cmd/migrate-version').default)
     .option('input', {
       describe: 'the input to initilize smart contract',
       default: '{}',

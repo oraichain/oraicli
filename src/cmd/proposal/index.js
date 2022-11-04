5d712b42ffa3f086307629d4ed58b81d9c3f1208
ducphamle2 <ducphamle212@gmail.com>
2022-11-04 13:23:18 -0700

    add create proposal cosmjs

--- src/cmd/proposal/index.js
+++ src/cmd/proposal/index.js
@@ -3,6 +3,7 @@ export default async (yargs: Argv) => {
   yargs
     .usage('usage: $0 provider <command> [options]')
     .command('create-proposal', 'Create a new proposal for the network', require('./cmd/create-proposal').default)
+    .command('create-proposal-cosmjs', 'Create a new proposal for the network cosmjs', require('./cmd/create-proposal-cosmjs').default)
     .command('create-proposal-ibc', 'Create a new proposal for ibc', require('./cmd/create-proposal-ibc').default)
     .command('deposit', 'Deposit funds into the proposal', require('./cmd/deposit').default)
     .command('vote', 'Vote for a proposal', require('./cmd/vote').default)
